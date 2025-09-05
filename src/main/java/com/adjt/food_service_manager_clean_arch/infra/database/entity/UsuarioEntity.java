ea8b5620d0216dd59afed522052892cfc622e422
Cauana <cauana.dias@hotmail.com>
2025-09-04 21:54:34 -0300

    wip: inicio infra usuario

---
+++ src/main/java/com/adjt/food_service_manager_clean_arch/infra/database/entity/UsuarioEntity.java
@@ -0,0 +1,25 @@
+package com.adjt.food_service_manager_clean_arch.infra.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name="usuarios")
+@Data
+@Builder
+@AllArgsConstructor
+public class UsuarioEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String senha;
+    private String tipoUsuario;
+
+}
+
