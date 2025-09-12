226e5562cc2e31bfc47c8ee3c18ba595411d14ba
cauana <cauana.dias@hotmail.com>
2025-09-11 22:20:05 -0300

    fix: corrige resposta restaurante

--- src/main/java/com/adjt/food_service_manager_clean_arch/infra/web/rest/RestauranteApiController.java
+++ src/main/java/com/adjt/food_service_manager_clean_arch/infra/web/rest/RestauranteApiController.java
@@ -3,6 +3,7 @@ package com.adjt.food_service_manager_clean_arch.infra.web.rest;
 import com.adjt.food_service_manager_clean_arch.core.domain.Restaurante;
 import com.adjt.food_service_manager_clean_arch.core.domain.Usuario;
 import com.adjt.food_service_manager_clean_arch.core.dto.CriarRestauranteDto;
+import com.adjt.food_service_manager_clean_arch.core.dto.RespostaRestauranteDto;
 import com.adjt.food_service_manager_clean_arch.core.usecase.CadastrarRestauranteUseCaseImpl;
 
 
@@ -19,19 +20,20 @@ public class RestauranteApiController {
     private final CadastrarRestauranteUseCaseImpl restauranteController;
 
     @PostMapping
-    public Restaurante criarRestaurante(@RequestBody CriarRestauranteDto restauranteDto) {
+    public RespostaRestauranteDto criarRestaurante(@RequestBody CriarRestauranteDto restauranteDto) {
         Restaurante restaurante = restauranteController.criarRestaurante(restauranteDto);
         log.info("Restaurante criado com ID: {}, nome: {}, dono: {}", restaurante.getId(), restaurante.getNome(), restaurante.getDonoRestaurante().getNome());
-        return restaurante;
+        return map(restaurante);
     }
 
-    public CriarRestauranteDto map(Restaurante restaurante) {
+    public RespostaRestauranteDto map(Restaurante restaurante) {
 
-		return CriarRestauranteDto.builder()
+		return RespostaRestauranteDto.builder()
+                .id(restaurante.getId())
 				.nome(restaurante.getNome())
 				.endereco(restaurante.getEndereco())
 				.tipoCozinha(restaurante.getTipoCozinha())
-				.idDonoRestaurante(restaurante.getDonoRestaurante().getId())
+				.nomeDonoRestaurante(restaurante.getDonoRestaurante().getNome())
 				.build();
 	}
 }
