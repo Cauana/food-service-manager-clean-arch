76d992b644fbb22dcec02b4924efc9ed00172f4d
Wellington Feijo Da Costa <wellingtonfc@hotmail.com>
2025-10-09 16:10:36 -0300

    Classe DeletarUsuarioUseCaseImpl com método deletarUsuario e validação

--- src/main/java/com/adjt/food_service_manager_clean_arch/core/usecase/usuario/DeletarUsuarioUseCaseImpl.java
+++ src/main/java/com/adjt/food_service_manager_clean_arch/core/usecase/usuario/DeletarUsuarioUseCaseImpl.java
@@ -1,4 +1,23 @@
 package com.adjt.food_service_manager_clean_arch.core.usecase.usuario;
 
+import com.adjt.food_service_manager_clean_arch.core.domain.Usuario;
+import com.adjt.food_service_manager_clean_arch.core.gateway.UsuarioGateway;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
 public class DeletarUsuarioUseCaseImpl {
+
+    private final UsuarioGateway usuarioGateway;
+
+    public void deletarUsuario(Long id){
+        Usuario usuario = usuarioGateway.buscarPorId(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Usuário não encontrado."
+                ));
+        usuarioGateway.deletar(usuario);
+    }
 }
