7c5c5b72ceed9947f74622dc776e0a3721339bdd
Wellington Feijo Da Costa <wellingtonfc@hotmail.com>
2025-10-09 14:27:35 -0300

    Classe para deletar tipo usuário e validação

--- src/main/java/com/adjt/food_service_manager_clean_arch/core/usecase/tipousuario/DeletarTipoUsuarioUseCaseImpl.java
+++ src/main/java/com/adjt/food_service_manager_clean_arch/core/usecase/tipousuario/DeletarTipoUsuarioUseCaseImpl.java
@@ -1,4 +1,27 @@
 package com.adjt.food_service_manager_clean_arch.core.usecase.tipousuario;
 
-public class DeletarTipoUsuarioUseCaseImpl {
+import com.adjt.food_service_manager_clean_arch.core.domain.TipoUsuario;
+import com.adjt.food_service_manager_clean_arch.core.gateway.TipoUsuarioGateway;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class DeletarTipoUsuarioUseCaseImpl implements DeletarTipoUsuarioUseCase{
+
+    private final TipoUsuarioGateway tipoUsuarioGateway;
+
+    public DeletarTipoUsuarioUseCaseImpl(TipoUsuarioGateway tipoUsuarioGateway) {
+        this.tipoUsuarioGateway = tipoUsuarioGateway;
+    }
+
+    @Override
+    public void deletar(Long id){
+        TipoUsuario existente = tipoUsuarioGateway.buscarPorId(id)
+                .orElseThrow(() ->new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Tipo de usuário com id " + id + " não encontrado."
+                ));
+        tipoUsuarioGateway.deletar(existente);
+    }
 }
