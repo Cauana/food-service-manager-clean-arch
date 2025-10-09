4b2187c6a09b8f787889f63c9f85ca3aa433c9ca
Wellington Feijo Da Costa <wellingtonfc@hotmail.com>
2025-10-09 10:53:23 -0300

    Criação interface AtualizarTipoUsuarioUseCase

--- src/main/java/com/adjt/food_service_manager_clean_arch/core/usecase/tipousuario/AtualizarTipoUsuarioUseCase.java
+++ src/main/java/com/adjt/food_service_manager_clean_arch/core/usecase/tipousuario/AtualizarTipoUsuarioUseCase.java
@@ -1,4 +1,7 @@
 package com.adjt.food_service_manager_clean_arch.core.usecase.tipousuario;
 
+import com.adjt.food_service_manager_clean_arch.core.domain.TipoUsuario;
+
 public interface AtualizarTipoUsuarioUseCase {
+    TipoUsuario atualizar(Long id, TipoUsuario tipoUsuario);
 }
