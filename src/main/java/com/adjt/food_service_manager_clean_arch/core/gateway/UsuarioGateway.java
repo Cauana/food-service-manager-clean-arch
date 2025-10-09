1969e33a59554470284a2cd2fe88e072f45f3ee4
Wellington Feijo Da Costa <wellingtonfc@hotmail.com>
2025-10-09 16:07:34 -0300

    Adicionado metodo salvar em UsuarioGateway

--- src/main/java/com/adjt/food_service_manager_clean_arch/core/gateway/UsuarioGateway.java
+++ src/main/java/com/adjt/food_service_manager_clean_arch/core/gateway/UsuarioGateway.java
@@ -6,9 +6,10 @@ import com.adjt.food_service_manager_clean_arch.core.domain.Usuario;
 
 public interface UsuarioGateway {
     Usuario criar(Usuario usuario);
+    Usuario salvar(Usuario usuario);
     Optional<Usuario> buscarPorId(Long id);
     Optional<Usuario> buscarPorLogin(String login);
     List<Usuario> buscarTodosUsuarios();
     Optional<Usuario> buscarPorCpf(String cpf);
-
+    void deletar(Usuario usuario);
 }
