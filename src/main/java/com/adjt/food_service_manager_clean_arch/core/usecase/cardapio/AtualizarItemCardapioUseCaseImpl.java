897637f3ae42576ceb0a7e00b2f1b97c4dd23eba
Wellington Feijo Da Costa <wellingtonfc@hotmail.com>
2025-10-10 16:42:19 -0300

    ajuste regra validação ao atualizar cardapio

--- src/main/java/com/adjt/food_service_manager_clean_arch/core/usecase/cardapio/AtualizarItemCardapioUseCaseImpl.java
+++ src/main/java/com/adjt/food_service_manager_clean_arch/core/usecase/cardapio/AtualizarItemCardapioUseCaseImpl.java
@@ -3,6 +3,7 @@ package com.adjt.food_service_manager_clean_arch.core.usecase.cardapio;
 import com.adjt.food_service_manager_clean_arch.core.domain.ItemCardapio;
 import com.adjt.food_service_manager_clean_arch.core.dto.CriarItemCardapioDto;
 import com.adjt.food_service_manager_clean_arch.core.gateway.ItemCardapioGateway;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,24 @@ import org.springframework.web.server.ResponseStatusException;
 public class AtualizarItemCardapioUseCaseImpl {
     private final ItemCardapioGateway itemCardapioGateway;
 
-    public ItemCardapio atualizar (Long id, CriarItemCardapioDto cardapioDto){
+    public ItemCardapio atualizar (Long id, CriarItemCardapioDto cardapioDto, HttpSession session){
         ItemCardapio existente = itemCardapioGateway.buscarPorId(id)
                 .orElseThrow(()-> new ResponseStatusException(
                         HttpStatus.NOT_FOUND,"Item do cardápio não encontrado"
                 ));
+
+        //Apenas dono do restaurante poderá alterar
+        String tipoUsuario = (String)session.getAttribute("tipoUsuario");
+        String cpfUsuario = (String) session.getAttribute("cpfUsuario");
+
+        if(!"DONO_RESTAURANTE".equals(tipoUsuario) ||
+        !existente.getRestaurante().getDonoRestaurante().getCpf().equals(cpfUsuario)){
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN,
+                    "Somente o dono do restaurante pode atualizar este item do cardápio"
+            );
+        }
+
         existente.setNome(cardapioDto.getNome());
         existente.setDescricao(cardapioDto.getDescricao());
         existente.setPreco(cardapioDto.getPreco());
